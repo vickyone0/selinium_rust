8f08628f701e268db6a416a65c10a9684703bd4c
vickyone0 <vigneshs0.618@gmail.com>
2025-06-30 01:04:45 -0400

    wiki test

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,32 @@
-use thirtyfour::prelude::*;
+use fantoccini::{ClientBuilder, Locator};
+use serde_json::{Map, Value};
 
 #[tokio::main]
-async fn main() -> WebDriverResult<()> {
-    // Start WebDriver (e.g., chromedriver --port=4444)
-    let caps = DesiredCapabilities::chrome();
-    let driver = WebDriver::new("http://localhost:9515", caps).await?;
+async fn main() -> Result<(), fantoccini::error::CmdError> {
+    // Build the capabilities as a Map<String, Value>
+    let mut caps = Map::new();
+    let mut chrome_opts = Map::new();
+    chrome_opts.insert("binary".to_string(), Value::String("/usr/bin/brave".to_string())); // Update path if needed
+    caps.insert(
+        "goog:chromeOptions".to_string(),
+        Value::Object(chrome_opts),
+    );
 
-    // Navigate to URL
-    driver.get("https://www.rust-lang.org").await?;
+    let c = ClientBuilder::native()
+        .capabilities(caps)
+        .connect("http://localhost:9515")
+        .await
+        .expect("failed to connect to WebDriver");
 
-    // Find element
-    let elem = driver.find_element(By::Id("some-id")).await?;
-    
-    // Type text
-    elem.send_keys("Rust").await?;
-    
-    // Click element
-    elem.click().await?;
+    c.goto("https://en.wikipedia.org/wiki/Foobar").await?;
+    let url = c.current_url().await?;
+    assert_eq!(url.as_ref(), "https://en.wikipedia.org/wiki/Foobar");
 
-    // Get page title
-    let title = driver.title().await?;
-    println!("Page title is: {}", title);
+    c.find(Locator::Css(".mw-disambig")).await?.click().await?;
+    c.find(Locator::LinkText("Foo Lake")).await?.click().await?;
 
-    // Always explicitly close the browser
-    driver.quit().await?;
+    let url = c.current_url().await?;
+    assert_eq!(url.as_ref(), "https://en.wikipedia.org/wiki/Foo_Lake");
 
-    Ok(())
+    c.close().await
 }
